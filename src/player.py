193c4344d3c68d1ed648f1fe638ea840047178f7
Hill-0xFF <leadlevelprogrammer@gmail.com>
2022-12-20 16:01:53 -0500

    [dev]: Implemented raycasting system v.0.0.1

--- src/player.py
+++ src/player.py
@@ -61,14 +61,18 @@ class Player:
             self.y += dy
     
     def draw(self):
-        pg.draw.line(self.game.screen, 'red', (self.x * 100, self.y * 100),
-        (self.x * 100 + WIDTH * math.cos(self.angle),
-        self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        # pg.draw.line(self.game.screen, 'red', (self.x * 100, self.y * 100),
+        # (self.x * 100 + WIDTH * math.cos(self.angle),
+        # self.y * 100 + WIDTH * math.sin(self.angle)), 2)
         
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
 
     def update(self):
         self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
 
     @property
     def map_pos(self):
---
+++ src/raycasting.py
@@ -0,0 +1,62 @@
+import pygame as pg
+# from player import *
+from settings import *
+import math
+
+class Raycasting:
+    def __init__(self, game):
+        self.game = game
+
+    def raycast(self):
+        pos_x, pos_y = self.game.player.pos
+        tilePosX_map, tilePosY_map = self.game.player.map_pos
+        ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        for ray in range (NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            # VERTICAL
+            x_vert, dx = (tilePosX_map + 1, 1) if cos_a > 0 else (tilePosX_map - 1e-6, -1)
+            depth_vert = (x_vert - pos_x) / cos_a
+            y_vert = pos_y + depth_vert * sin_a
+
+            delta_depth = dx / cos_a
+            dy = delta_depth * sin_a
+
+            for i in range(MAX_DEPTH):
+                tile_vert = int(x_vert), int(y_vert)
+                if tile_vert in self.game.map.world_map:
+                    break
+                x_vert += dx
+                y_vert += dy
+                depth_vert += delta_depth
+            
+            # HORIZONTAL
+            y_hor, dy = (tilePosY_map + 1, 1) if sin_a > 0 else (tilePosY_map - 1e-6, -1)
+            depth_hor = (y_hor - pos_y) / sin_a
+            x_hor = pos_x + depth_hor * cos_a
+
+            delta_depth = dy / sin_a
+            dx = delta_depth * cos_a
+
+            for i in range(MAX_DEPTH):
+                tile_hor = int(x_hor), int(y_hor)
+                if tile_hor in self.game.map.world_map:
+                    break
+                x_hor += dx
+                y_hor += dy
+                depth_hor += delta_depth
+            
+            # determing the DEPTH
+            if depth_vert < depth_hor:
+                depth = depth_vert
+            else: 
+                depth = depth_hor
+
+            #Casting the rays
+            pg.draw.line(self.game.screen, 'white', (100 * pos_x, 100 * pos_y),
+            (100 * pos_x + 100 * depth * cos_a, 100 * pos_y + 100 * depth * sin_a), 2)
+            ray_angle += DELTA_ANGLE
+
+    def update(self):
+        self.raycast()
